bc1e66bd8e23d59b1f6ebc92795e8fbc537ddd7e
Christopher Diffley <diffley@gmail.com>
2018-03-13 20:03:06 -0400

    updates to ubuntu - not a working script - but working instructions (mostly)

--- provisioning/ubuntuWithGUI.sh
+++ provisioning/ubuntuWithGUI.sh
@@ -1,10 +1,22 @@
 # https://linode.com/docs/applications/remote-desktop/install-vnc-on-ubuntu-16-04/
 # https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-vnc-on-ubuntu-16-04
 
-apt-get update
-apt-get upgrade
-apt-get install ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
-apt-get install vnc4server
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
+apt-get install -y vnc4server
+
+# need to figure out how to do this automatically to include password setting, etc
+# adduser difflch
+# usermod -aG sudo difflch
+# su - difflch
+# vncpasswd
+# vncserver
+# vncserver -kill :%i
+# crontab -e
+# add @reboot /usr/bin/vncserver :%i
+# exit
+# repeat for other users
 
 
 # GUAC
